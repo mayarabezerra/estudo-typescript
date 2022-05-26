6a45df04c96f8dbbd836f18262cec567d43072dd
Mayara Bezerra do Nascimento <mayara.do.nascimento@avanade.com>
2022-05-26 13:15:43 -0300

    finalização da lógica de código: curso 02

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -14,4 +14,12 @@ export class Negociacao {
         return data;
     }
 
+    public static criaDe (dataString: string, quantidadeString: string, valorString: string): Negociacao {
+        const exp = /-/g;
+        const date = new Date(dataString.replace(exp, ','));
+        const quantidade = parseInt(quantidadeString);
+        const valor = parseInt(valorString);
+        return new Negociacao(date, quantidade, valor);
+    }
+
 }
